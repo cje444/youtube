88285c61d161886373f14df5c4cbe499020a70e5
cje444 <cje444@naver.com>
2021-04-10 17:03:02 -0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import VideoList from "./component/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]); //초기값은 빈배열
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ&part=snippet&chart=mostPopular&maxResults=25&=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+
+    console.log("useEffect");
+  }, []); //[]배열안의 내용이 업데이트 될때마다 useEffect가 호출됨
+
+  return <VideoList videos={videos} />;
+}
+
+export default App;
