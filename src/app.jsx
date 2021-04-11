f1a4dd990aa82d26891970166df41cd117c313e9
cje444 <cje444@naver.com>
2021-04-11 11:04:53 -0400

    refectoring

--- src/app.jsx
+++ src/app.jsx
@@ -3,41 +3,18 @@ import { useEffect, useState } from "react";
 import VideoList from "./component/video_list/video_list";
 import SearchHeader from "./component/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]); //초기값은 빈배열
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ&part=snippet&maxResults=25&q=${query}&type=video&=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube //
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
-
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ&part=snippet&chart=mostPopular&maxResults=25&=AIzaSyDCQBFYD9fDpFiRyZ-fXH108l5yZeBcQpQ",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
-
-    console.log("useEffect");
+    youtube //
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); //[]배열안의 내용이 업데이트 될때마다 useEffect가 호출됨
 
   return (
